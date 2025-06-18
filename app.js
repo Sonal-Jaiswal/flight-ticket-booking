ab73364f0cb4bad26c5c3cc52dad574f79208b36
Sonal Jaiswal <vsonaljaiswal@gmail.com>
2025-06-19 01:24:37 +0530

    a

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ app.use(session({
   resave: false,
   saveUninitialized: false,
   cookie: {
-    secure: process.env.NODE_ENV === 'production', // Set to true in production
+    secure: true //process.env.NODE_ENV === 'production', // Set to true in production
     maxAge: 24 * 60 * 60 * 1000 // 24 hours
   }
 }));
